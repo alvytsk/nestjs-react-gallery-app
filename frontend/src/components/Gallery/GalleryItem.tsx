7ca9a1ca8238f027daf20027592c5dd0c811d4da
Alexey Vymyatnin <avymiatnin@aspirity.com>
2022-11-05 10:02:10 +0700

    Add overlay info panel to gallery item

--- frontend/src/components/Gallery/GalleryItem.tsx
+++ frontend/src/components/Gallery/GalleryItem.tsx
@@ -4,7 +4,10 @@ import { GalleryFileDTO } from '~/types/gallery';
 const GalleryItem: React.FC<{ file: GalleryFileDTO }> = ({ file }) => {
   return (
     <div className="gallery__item">
-      <img src={file.url} alt={file.name} />{' '}
+      <img src={file.url} alt={file.name} />
+      <div className="item__overlay">
+        <div className="item__overlay-text">{file.name}</div>
+      </div>
     </div>
   );
 };
